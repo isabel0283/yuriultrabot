b0ebc4f8a363013b24f390c2b3d4991d95e71609
isabel0283 <53575044+isabel0283@users.noreply.github.com>
2019-08-19 18:10:13 -0500

    Update KitchenSinkController.java

--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/KitchenSinkController.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/KitchenSinkController.java
@@ -94,9 +94,8 @@ import lombok.extern.slf4j.Slf4j;
 //import org.json.Document;
 import org.json.JSONObject;
 import org.json.JSONTokener;
-import org.jsoup.Jsoup;
-//import org.jsoup.*;
 import org.jsoup.nodes.Document;
+import org.jsoup.Jsoup;
 //import org.jsoup.select.*;
 
 @Slf4j
