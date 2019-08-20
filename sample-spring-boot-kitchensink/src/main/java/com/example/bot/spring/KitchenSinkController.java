f44c288c93519bcaabd8af1653dd2676fe046ed0
isabel0283 <53575044+isabel0283@users.noreply.github.com>
2019-08-20 12:03:58 -0500

    Update KitchenSinkController.java

--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/KitchenSinkController.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/KitchenSinkController.java
@@ -529,9 +529,9 @@ public class KitchenSinkController {
                 String keyword = "panama";
                 keyword = keyword.replace(" ", "+");
                 String url = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&order=rating&q=" + keyword + "&key=AIzaSyCIky_AwVV1XNvChlx5Dlq517RjJFs_yIA";
-                Document doc = Jsoup.connect(url).timeout(5000).get();
                 log.info("Returns echo message {}: {}", replyToken, text);
                 this.replyText(replyToken, "Content of " + url);
+                Document doc = Jsoup.connect(url).timeout(5000).get();
                 String getJson = doc.text();
                 JSONObject jsonObject = (JSONObject) new JSONTokener(getJson).nextValue();
                 this.replyText(replyToken, jsonObject.getString("videoId"));
