825c45217623feade2e04cdcfb67640b7adf3ec4
isabel0283 <53575044+isabel0283@users.noreply.github.com>
2019-08-20 17:05:43 -0500

    Hhhj

--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/KitchenSinkController.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/KitchenSinkController.java
@@ -513,7 +513,7 @@ public class KitchenSinkController {
             case "yuri":
                 int randNumber = (int)(Math.random() * 50 + 1);
                 log.info("Returns echo message {}: {}", replyToken, text);
-                this.replyText(replyToken, "Uh?" + Integer.toStr(randNumber));
+                this.replyText(replyToken, "Uh?" + Integer.toString(randNumber));
                 break;
             case "food":
                 log.info("Returns echo message {}: {}", replyToken, text);
