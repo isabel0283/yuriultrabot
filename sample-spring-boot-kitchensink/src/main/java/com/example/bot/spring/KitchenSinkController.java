b9ded4b3783da368967349b3fd08ca26352e1bb2
isabel0283 <53575044+isabel0283@users.noreply.github.com>
2019-08-19 22:33:47 -0500

    Tre

--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/KitchenSinkController.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/KitchenSinkController.java
@@ -534,7 +534,7 @@ public class KitchenSinkController {
                 Document doc = Jsoup.connect(url).timeout(10 * 1000).get();
                 String getJson = doc.text();
                 JSONObject jsonObject = (JSONObject) new JSONTokener(getJson).nextValue();
-                this.replyText(replyToken, url);  
+                this.replyText(replyToken, url);
                 this.replyText(replyToken, jsonObject.getString("videoId"));
                 break;
             default:
