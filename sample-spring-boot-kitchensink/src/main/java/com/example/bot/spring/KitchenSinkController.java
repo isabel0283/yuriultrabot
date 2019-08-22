3fa9ecac1b733d6da10a4aee0474c856fcb9fd70
isabel0283 <53575044+isabel0283@users.noreply.github.com>
2019-08-21 19:27:53 -0500

    Test

--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/KitchenSinkController.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/KitchenSinkController.java
@@ -554,7 +554,7 @@ public class KitchenSinkController {
                 JSONObject jsonObject = (JSONObject) new JSONTokener(getJson).nextValue();
                 //JSONObject object = jsonObject.getJSONObject("items");
                 //JSONArray subArray = object.getJSONArray("id");
-                String video = jsonObject.getString();
+                String video = jsonObject.getString("etag");
                 //String video = subArray.getJSONObject(0).getString("videoId").toString();
                 this.replyText(replyToken, video);
                 break;
