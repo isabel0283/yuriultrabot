714cc2b8e9311d707904bf787e2e612253837ce4
isabel0283 <53575044+isabel0283@users.noreply.github.com>
2019-09-02 21:47:35 -0500

    Message

--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/KitchenSinkController.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/KitchenSinkController.java
@@ -451,7 +451,7 @@ public class KitchenSinkController {
                 break;
             case "sugestYoutubeYuri":
                 log.info("Returns echo message {}: {}", replyToken, text);
-                String message = "You could ask me to search for a video. For example: Yuri Youtube kurt cobain";
+                String message = "You could ask me to search for a video, like this: Yuri Youtube kurt cobain";
                 this.replyText(replyToken, message);
                 break;
             case "youtubeYuri":
