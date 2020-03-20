a8748aa1fd602fb2c2b3f7f6afc736c4d9482b7f
isabel0283 <53575044+isabel0283@users.noreply.github.com>
2020-03-19 23:32:03 -0500

    Update KitchenSinkController.java

--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/KitchenSinkController.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/KitchenSinkController.java
@@ -299,7 +299,7 @@ public class KitchenSinkController {
                     break;
                 }
                 keyword = keyword.replace(" ", "+");
-                String url = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&order=relevance&q=" + keyword + "&key=AIzaSyCIky_AwVV1XNvChlx5Dlq517RjJFs_yIA";
+                String url = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&order=relevance&q=" + keyword + "&key=AIzaSyAkW1C_AMaU60nOpJunpMAu9RLq09l84Ms";
                 Document result = Jsoup.connect(url)
                     .userAgent("Mozilla")
                     .timeout(3000)
