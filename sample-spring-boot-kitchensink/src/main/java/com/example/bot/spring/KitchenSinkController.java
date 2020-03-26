ff535953a7f2c23f97ebfcdddfc9362a5755b609
isabel0283 <53575044+isabel0283@users.noreply.github.com>
2020-03-26 15:54:03 -0500

    Update KitchenSinkController.java

--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/KitchenSinkController.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/KitchenSinkController.java
@@ -384,10 +384,9 @@ public class KitchenSinkController {
                 String wreport = "We have " + yourConditionDesc + " in " + yourCity + ", " + yourCountry;
                 wreport = wreport + ". The current temperature is " + yourTemp;
                 wreport = wreport + "°C, and humidity is " + yourHumidity + "%.";
-                //this.reply(ImageMessage(cloudIconurl, cloudIconurl));
-                //this.reply(ImageMessage(flagIconurl, flagIconurl));
+                this.reply(new ImageMessage(cloudIconurl, cloudIconurl));
+                //this.reply(new ImageMessage(flagIconurl, flagIconurl));
                 this.replyText(replyToken, wreport + cloudIconurl + flagIconurl);
-                this.reply(ImageMessage(cloudIconurl, cloudIconurl));
                 //this.reply(replyToken,
                 //    Arrays.asList(new TextMessage(wreport),
                 //    new ImageMessage(cloudIconurl, cloudIconurl),
