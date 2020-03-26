2675cb11ff4a6133bbece991f80a96e3bfe6c164
isabel0283 <53575044+isabel0283@users.noreply.github.com>
2020-03-26 16:16:40 -0500

    Update KitchenSinkController.java

--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/KitchenSinkController.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/KitchenSinkController.java
@@ -384,14 +384,8 @@ public class KitchenSinkController {
                 String wreport = "We have " + yourConditionDesc + " in " + yourCity + ", " + yourCountry;
                 wreport = wreport + ". The current temperature is " + yourTemp;
                 wreport = wreport + "°C, and humidity is " + yourHumidity + "%.";
-                this.reply(Arrays.asList(new ImageMessage(cloudIconurl, cloudIconurl),
-                    new ImageMessage(flagIconurl, flagIconurl)));
-                //this.reply(new ImageMessage(flagIconurl, flagIconurl));
-                this.replyText(replyToken, wreport + cloudIconurl + flagIconurl);
-                //this.reply(replyToken,
-                //    Arrays.asList(new TextMessage(wreport),
-                //    new ImageMessage(cloudIconurl, cloudIconurl),
-                //    new ImageMessage(flagIconurl, flagIconurl)));
+                this.reply(replyToken, new ImageMessage(flagIconurl, flagIconurl));
+                //this.replyText(replyToken, wreport);
                 break;
             case "yuri show me something pretty": {
                 log.info("Invoking 'profile' command: source:{}",
