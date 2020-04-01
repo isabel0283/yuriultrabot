f935338cfbb8e477d662dc5693c9b2ee079f07cb
isabel0283 <53575044+isabel0283@users.noreply.github.com>
2020-04-01 12:25:47 -0500

    Update KitchenSinkController.java

--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/KitchenSinkController.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/KitchenSinkController.java
@@ -642,11 +642,11 @@ public class KitchenSinkController {
                 break;
             case "stopYuri":
                 log.info("Returns echo message {}: {}", replyToken, text);
-                TimeUnit.MINUTES.sleep(1);
+                Thread.sleep(300000);
                 this.replyText(replyToken, "I'm here again!");
                 break;
             case "hahaha": {
-                myrandInt = rand.nextInt(15);
+                myrandInt = rand.nextInt(20);
                 log.info("Returns echo message {}: {}", replyToken, text);
                 strRandom = "" + myrandInt;
                 switch (strRandom) {
