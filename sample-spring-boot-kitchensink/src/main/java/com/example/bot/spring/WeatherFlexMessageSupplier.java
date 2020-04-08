fe4c3c48ed88c7854aeaa6ea34d6bcb93f2b0c25
isabel0283 <53575044+isabel0283@users.noreply.github.com>
2020-04-08 12:32:49 -0500

    Update WeatherFlexMessageSupplier.java

--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/WeatherFlexMessageSupplier.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/WeatherFlexMessageSupplier.java
@@ -75,8 +75,8 @@ public class WeatherFlexMessageSupplier implements Supplier<FlexMessage> {
     }
 
     private Box createBodyBlock() {
-        KitchenSinkController weather = new KitchenSinkController();
-        String location = weather.testwreportLocation;
+        //KitchenSinkController weather = new KitchenSinkController();
+        String location = KitchenSinkController.testwreportLocation;
         final Text title = Text
                    .builder()
                    .text(location)
