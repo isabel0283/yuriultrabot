2d20e6e4ea6b15a85aecd2b39b219c4db868ee9b
isabel0283 <53575044+isabel0283@users.noreply.github.com>
2020-11-06 11:37:02 -0500

    Update KitchenSinkController.java

--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/KitchenSinkController.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/KitchenSinkController.java
@@ -434,6 +434,12 @@ public class KitchenSinkController {
          } else {
                 text = "stopYuri";
          }
+         intIndex = strOrig.indexOf("liar");
+         if (intIndex == -1) {
+                intIndex = 0;
+         } else {
+                text = "liar";
+         }
          intIndex = strOrig.indexOf("hello");
          if (intIndex == -1) {
                 intIndex = 0;
@@ -644,6 +650,32 @@ public class KitchenSinkController {
                            break;
                        case "4":
                            message = "I don't think you would like this https://www.youtube.com/watch?v=P7VK09LpgW8";
+                           break;
+                       default:
+                           break;
+                }
+                this.replyText(replyToken, message);
+                break;
+            }
+            case "liar": {
+                log.info("Returns echo message {}: {}", replyToken, text);
+                myrandInt = rand.nextInt(10);
+                strRandom = "" + myrandInt;
+                switch (strRandom) {
+                       case "0":
+                           message = "Yes, liar!";
+                           break;
+                       case "1":
+                           message = "Truth is relative!";
+                           break;
+                       case "2":
+                           message = "You'll see who's the liar!";
+                           break;
+                       case "3":
+                           message = "Time will tell...";
+                           break;
+                       case "4":
+                           message = "Ask someone else, and you'll know the truth...";
                            break;
                        default:
                            break;
